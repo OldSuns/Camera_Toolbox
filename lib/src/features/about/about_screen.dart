5a7fdfcf1d9c3cf5dca463815d84777354198024
OldSuns <1239502201@qq.com>
2025-07-25 03:09:52 +0800

    v110_Patch1

--- lib/src/features/about/about_screen.dart
+++ lib/src/features/about/about_screen.dart
@@ -108,7 +108,7 @@ class _AboutScreenState extends State<AboutScreen> {
               ),
               const SizedBox(height: 8),
               Text(
-                '版本 V1.0.6${_patchNumber == null ? '' : ' Patch $_patchNumber'}',
+                '版本 V1.1.0${_patchNumber == null ? '' : ' Patch $_patchNumber'}',
                 style: const TextStyle(fontSize: 16, color: Colors.grey),
               ),
               const SizedBox(height: 24),
